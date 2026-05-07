1aa6526b998eba7311c868f1ae5989e12369a534
BLAKEJEWELL <bjewell1833@gmail.com>
2026-05-07 09:38:23 -0400

    "finished" schedule_item class. might need to add/change stuff later. added a print funtion to schedule class. tested and it works.

--- coarse_schedule_system/schedule.py
+++ coarse_schedule_system/schedule.py
@@ -4,3 +4,18 @@ from schedule_item import Schedule_Item
 class Schedule:
     def __init__(self):
 
+    def print_h(self):
+        print(
+            f"{'subject':<8}"
+            f"{'Catalog':<8}"
+            f"{'Section':<8}"
+            f"{'Component':<8}"
+            f"{'Session':<8}"
+            f"{'Units':<8}"
+            f"{'TotEnrl':<8}"
+            f"{'CapEnrl':<8}"
+            f"{'Instructor':<8}"
+            
+            )
+
+#call get_key
--- coarse_schedule_system/schedule_item.py
+++ coarse_schedule_system/schedule_item.py
@@ -2,4 +2,29 @@ from dataclasses import dataclass
 
 @dataclass
 class Schedule_Item:
-    def __init__(self):
+    subject: str
+    catalog: str
+    section: str
+    component: str
+    session: str
+    units: int
+    tot_enrl: int
+    cap_enrl: int
+    instructor: str
+
+def print(self):
+    print(
+        f"{self.subject:<8}"
+        f"{self.catalog:<8}"
+        f"{self.section:<8}"
+        f"{self.componetn:<8}"
+        f"{self.session:<8}"
+        f"{self.units:<8}"
+        f"{self.totenrl:<8}"
+        f"{self.capenrl:<8}"
+        f"{self.instructor:<8}"
+        
+        )
+
+def get_key(self):
+    return f"{self.subject}_{self.catalog}_{self.section}"
